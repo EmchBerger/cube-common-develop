aeca36c7f08d6673f7ae60deef98826ba9b5668e
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-07-21 16:53:05 +0200

    [Test] assertions for filled array elements

--- src/Test/MoreAssertionsTrait.php
+++ src/Test/MoreAssertionsTrait.php
@@ -52,4 +52,47 @@ trait MoreAssertionsTrait
         };
         TestCase::assertSame(array(), array_filter($array, $isEmpty), $explication);
     }
+
+    /**
+     * Asserts the array contains only filled elements.
+     *
+     * Filled elements are values returned when a form field cot some value.
+     * Not filled: null (Entities, Selections), '' (Text), [] (MultiSelect)
+     *
+     * An empty array will pass.
+     *
+     * @param array  $array
+     * @param string $explication
+     */
+    public static function assertArrayFilledElementsOnly(array $array, $explication = null)
+    {
+        if (null === $explication) {
+            $explication = 'only filled elements';
+        }
+        $isNotFilled = function ($element) {
+            return '' !== $element && count($element) > 0;
+        };
+        TestCase::assertSame(array(), array_filter($array, $isNotFilled), $explication);
+    }
+
+    /**
+     * Asserts the array contains no filled elements.
+     *
+     * {@see self::assertArrayFilledElementsOnly()}
+     *
+     * An empty array will pass.
+     *
+     * @param array  $array
+     * @param string $explication
+     */
+    public static function assertArrayNoFilledElements(array $array, $explication = null)
+    {
+        if (null === $explication) {
+            $explication = 'no filled elements';
+        }
+        $isFilled = function ($element) {
+            return '' === $element || count($element) <= 0;
+        };
+        TestCase::assertSame(array(), array_filter($array, $isFilled), $explication);
+    }
 }
