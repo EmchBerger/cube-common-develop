addac2734d5f7e5c11707ddff36920cfb8f58b64
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2018-01-25 10:06:09 +0100

    [general] fix runSymfonyCommands, runs successfully now

--- src/runSymfonyCommands.php
+++ src/runSymfonyCommands.php
@@ -5,6 +5,7 @@ namespace CubeTools\CubeCommonDevelop;
 
 $_mainFile = !\debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
 if ($_mainFile) {
+    require_once __DIR__.'/SymfonyCommands.php';
     $r = SymfonyCommands::initCommands();
     echo $r;
 }
