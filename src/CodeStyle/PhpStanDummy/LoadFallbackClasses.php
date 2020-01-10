1999eeb2cdf52bb0a26565d5a9d83cc87ddc5dea
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2020-01-10 19:22:31 +0100

    [testing] add baseline for phpstan, inclusive command for generating

--- src/CodeStyle/PhpStanDummy/LoadFallbackClasses.php
+++ src/CodeStyle/PhpStanDummy/LoadFallbackClasses.php
@@ -1,16 +1,24 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    $namespacePrefix = 'CubeTools\CubeCommonDevelop\CodeStyle\PhpStanDummy\Classes';
-    $s = null;
+    $loadClass = function ($localName) use ($class) {
+        try {
+            $prefix = 'CubeTools\CubeCommonDevelop\CodeStyle\PhpStanDummy\Classes\\';
+            $s = class_alias($prefix.$localName, $class);
+        } catch (\Throwable $e) {
+            $msg = sprintf("Exception %s while loading dummy for '%s'\n", get_class($e), $class);
+            echo $msg;
+            throw $e;
+        }
+        if (false === $s) {
+            echo "failed to load dummy for '$class'\n";
+        }
+    };
     if ('PHPUnit\Framework\TestCase' === $class) {
-        $s = class_alias($namespacePrefix.'\PhpUnit\TestCase', $class);
+        $loadClass('PHPUnit\TestCase');
     } elseif ('Symfony\Bundle\FrameworkBundle\Test\KernelTestCase' === $class) {
-        $s = class_alias($namespacePrefix.'\SymfonyFrameworkTests\KernelTestCase', $class);
+        $loadClass('SymfonyFrameworkTests\KernelTestCase');
     } elseif ('Symfony\Bundle\FrameworkBundle\Test\WebTestCase' === $class) {
-        $s = class_alias($namespacePrefix.'\SymfonyFrameworkTests\WebTestCase', $class);
-    }
-    if (false === $s) {
-        echo "failed to load dummy for '$class'\n";
+        $loadClass('SymfonyFrameworkTests\WebTestCase');
     }
 });
