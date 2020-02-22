b55e3ff125b3a0b6a38b331b486a2fe18be15f18
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2020-02-22 10:48:28 +0100

    [CodeStyle] verify-twig-template... checks if all templates are used

--- src/CodeStyle/verify-twig-template-usage.sh
+++ src/CodeStyle/verify-twig-template-usage.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #
 # finds all (potential) usages of twig templates and checks if they exist
+#     after it checks if all existing templates are used
 
 set -o nounset -o errexit
 
-atExitReport () {
+atExit () {
     rc=$?
     if [ 0 -ne $rc ]
     then
@@ -12,9 +13,13 @@ atExitReport () {
         [ -n "$1" ] && printf ' on line %s' "$1"
         printf '\n'
     fi >&2
+    if [ -n "${usedTemplatesFile:-}" ]
+    then
+        rm "$usedTemplatesFile" || true
+    fi
 }
 
-trap 'atExitReport "${LINENO:-}"' EXIT
+trap 'atExit "${LINENO:-}"' EXIT
 
 noArgs=
 if [ 0 -eq $# ]
@@ -29,6 +34,13 @@ colorMatch() {
 
 matchD='"[^"][^"]*\.twig"'
 matchS="'[^'][^']*\\.twig'"
+
+if [ -n "$noArgs" ]
+then
+    usedTemplatesFile=$(mktemp --tmpdir twig-used-templates-XXXX)
+else
+    usedTemplatesFile=
+fi
 
 {
     grep -r --line-number '--include=*.php' '--include=*.twig' -e "$matchD" -e "$matchS" "$@"
@@ -50,8 +62,22 @@ do
         if ! [ -f "templates/$template" ]
         then
             printf 'not found in %s\n' "$grepMatch" | colorMatch "$template"
+        elif [ -n "$usedTemplatesFile" ]
+        then
+            echo "templates/$template" >> "$usedTemplatesFile"
         fi
     done
 done
 
 printf '%s\n' "$@" | grep ^src/ | xargs -r -d '\n' -- grep -r --include=*.php --line-number -e '@Template()' | sed 's/^/  unchecked in /'
+
+if [ -n "$usedTemplatesFile" ]
+then
+    find templates/ -name 'bundles' -type d -prune -o -name '*\.twig' -print | while read -r existingTemplate
+    do
+        if ! grep -q --fixed-strings --line-regexp -e "$existingTemplate" "$usedTemplatesFile"
+        then
+            echo "probably unused: $existingTemplate"
+        fi
+    done
+fi
