116c189f8c13b518be69454b05a65e7dfc32492d
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2020-05-27 16:45:11 +0200

    [CodeStyle] add comments to verify-twig-template...

--- src/CodeStyle/verify-twig-template-usage.sh
+++ src/CodeStyle/verify-twig-template-usage.sh
@@ -43,19 +43,23 @@ else
 fi
 
 {
+    # list all lines using templates and pass the info to the while loop
+
     grep -r --line-number '--include=*.php' '--include=*.twig' -e "$matchD" -e "$matchS" "$@"
     if [ -n "$noArgs" ]
     then
+        # when checking all (=noArgs), then also check for tempaltes in config files
         find config/ app/config/ -maxdepth 0 -print0 | xargs -0 -r -- grep -r --line-number '--include=*.yml' '--include=*.yaml' -e '\.twig($|[ "'"'])"
     fi
 } | while read -r grepMatch
 do
+    # grepMatch is an output line from grep, like: my/file:2:  line from file
     printf %s "$grepMatch" | grep -o -e "$matchD" -e "$matchS" -e '\b[^ ]*\.twig($|[ ])' | sed -e 's/^["'"']//" -e 's/[ "'"']$//" |
       # explain above:                   # get match only                                         # strip quotes (" or ') at start and end (or space at end)
       while read -r template
-    do
+    do # for each template found on the line:
         if printf %s "$template" | grep -q ^@ # starts with @
-        then
+        then # because we do not check bundles
             printf '  unchecked in %s\n' "$grepMatch" | colorMatch "$template"
             continue
         fi
@@ -63,18 +67,22 @@ do
         then
             printf 'not found in %s\n' "$grepMatch" | colorMatch "$template"
         elif [ -n "$usedTemplatesFile" ]
-        then
+        then # list template as used
             echo "templates/$template" >> "$usedTemplatesFile"
         fi
     done
 done
 
-printf '%s\n' "$@" | grep ^src/ | xargs -r -d '\n' -- grep -r --include=*.php --line-number -e '@Template()' | sed 's/^/  unchecked in /'
+printf '%s\n' "$@" | grep ^src/ | # list all files in src/ (from arguments)
+  xargs -r -d '\n' -- grep -r --include=*.php --line-number -e '@Template()' | # which use @Template(), so without arguments
+  sed 's/^/  unchecked in /' # are not checked
 
 if [ -n "$usedTemplatesFile" ]
 then
+    # list all existing templates, ...
     find templates/ -name 'bundles' -type d -prune -o -name '*\.twig' -print | while read -r existingTemplate
     do
+        # and check if it is used
         if ! grep -q --fixed-strings --line-regexp -e "$existingTemplate" "$usedTemplatesFile"
         then
             echo "probably unused: $existingTemplate"
