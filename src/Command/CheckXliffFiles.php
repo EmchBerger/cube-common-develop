95a582117a0b5038495e5061ee6212fce308136c
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2020-05-29 10:25:21 +0200

    [cleanup] cleanup CheckXliffFiles.php
    reorder imports
    
    by running: vendor/bin/php-cs-fixer fix -- src/Command/CheckXliffFiles.php

--- src/Command/CheckXliffFiles.php
+++ src/Command/CheckXliffFiles.php
@@ -2,12 +2,12 @@
 
 namespace CubeTools\CubeCommonDevelop\Command;
 
+use CubeTools\CubeCommonDevelop\CodeStyle\XliffFiles;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use CubeTools\CubeCommonDevelop\CodeStyle\XliffFiles;
 
 class CheckXliffFiles extends Command
 {
